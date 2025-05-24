421ed493ffc6033e21c4c5e1e2bdf24c0fd21b21
HoangPhuc6705 <tonguyenhoangphuc6705@gmail.com>
2025-05-24 11:48:50 +0700

    update

---
+++ src/main/java/GUI/Home/Home.java
@@ -0,0 +1,23 @@
+package GUI.Home;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Home extends JFrame {
+    public static final int WIDTH = 720;
+    public static final int HEIGHT = 480;
+
+    public Home() {
+        super("Order App");
+
+        Container container = this.getContentPane();
+        container.setLayout(new BorderLayout());
+
+
+
+        this.setSize(WIDTH, HEIGHT);
+        this.setLocationRelativeTo(null);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setVisible(true);
+    }
+}
---
+++ src/main/java/GUI/MenuBar/MenuBar.java
@@ -0,0 +1,9 @@
+package GUI.MenuBar;
+
+import javax.swing.*;
+
+public class MenuBar extends JMenuBar  {
+    public MenuBar() {
+
+    }
+}
